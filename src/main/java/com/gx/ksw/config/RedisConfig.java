2f4b53c7ea695a5bd5241117e9eb3d1b39157346
sunguangxu <sun_guang_xu@163.com>
2019-06-28 16:57:48 +0800

    增加缓存

---
+++ src/main/java/com/gx/ksw/config/RedisConfig.java
@@ -0,0 +1,43 @@
+package com.gx.ksw.config;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
+
+/**
+ * 自定义缓存的配置
+ * @author sgx
+ * @version V1.1.0
+ * @date 2019/6/28 15:31
+ * @since V1.1.0
+ */
+@Slf4j
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+	/**
+	 * 自定义缓存的Key
+	 * @return
+	 */
+	@Override
+	public KeyGenerator keyGenerator() {
+		return (target, method, params) -> {
+			StringBuilder sb = new StringBuilder();
+			sb.append(target.getClass().getName());
+			sb.append("-");
+			sb.append(method.getName());
+			if (params.length > 0) {
+				for (Object obj : params) {
+					sb.append("-");
+					sb.append(obj.toString());
+				}
+			}
+			log.info("缓存中的key:" + sb.toString());
+			return sb.toString();
+		};
+	}
+}
